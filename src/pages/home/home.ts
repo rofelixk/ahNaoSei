1e1316de8f230f19c90555df7eb386d5ed38acf1
rofelixk <ro.felix.k@gmail.com>
2017-05-24 17:54:36 -0300

    mudanças nas config

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController) {
 
-  }
+	}
+	places:any = [
+	{name: 'Barzin', icon: 'beer', color:'#f1c40f'},
+	{name: 'Parquin', icon: 'leaf', color:'#2ecc71'},
+	{name: 'Comidin', icon: 'restaurant', color:'#e74c3c'},
+	{name: 'Cafezin', icon: 'cafe', color:'#2980b9'}];
+	selectedPlace:any = 'leaf';
+	distance:any = 500;
 
 }
