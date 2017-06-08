f595b09fb808c1b9717aa786ef8a38c9d4ebff72
Natan Mann <mannnatan@gmail.com>
2017-06-08 00:11:00 -0300

    Criado variaveis Sass para o projeto. (beer, cafe, restaurant e leaf)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,12 +10,11 @@ export class HomePage {
 	constructor(public navCtrl: NavController) {
 
 	}
-	places:any = [
-	{name: 'Barzin', icon: 'beer', color:'#f1c40f'},
-	{name: 'Parquin', icon: 'leaf', color:'#2ecc71'},
-	{name: 'Comidin', icon: 'restaurant', color:'#e74c3c'},
-	{name: 'Cafezin', icon: 'cafe', color:'#2980b9'}];
-	selectedPlace:any = 'leaf';
-	distance:any = 500;
-
+	places: any = [
+		{ name: 'Barzin', icon: 'beer', color: '#f1c40f' },
+		{ name: 'Parquin', icon: 'leaf', color: '#2ecc71' },
+		{ name: 'Comidin', icon: 'restaurant', color: '#e74c3c' },
+		{ name: 'Cafezin', icon: 'cafe', color: '#2980b9' }];
+	selectedPlace: any = 'leaf';
+	distance: any = 500;
 }
